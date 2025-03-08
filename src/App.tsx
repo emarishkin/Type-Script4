d48a0b4ef7b1917ef95efdf095210d3c69a7b0aa
emarishkin <ee.marishkin@yandex.ru>
2025-03-08 16:55:43 +0400

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { useState } from "react"
 import Card from "./components/Card"
 import UserList from "./components/UserList"
 import {users} from './users'
 
 function App() {
+  const [click,setClick] = useState(false)
   return (
     <>
      <Card fontSize={30} width={100} height={100}><p style={{margin:'0 20px'}}>click</p>
-     <button style={{margin:'0 30px'}}>
+     <button onClick={()=>setClick(prev=>!prev)} style={{margin:'0 30px'}}>
        click
      </button>
-     
      </Card>
-     <UserList users={users}/>
+     
+     {click && <div><UserList users={users}/></div>}
+     
+     
+     
     </>
   )
 }
